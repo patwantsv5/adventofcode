49ab0a7eebdf9b39127c5079861a042542f22128
Pat Sri <patwantsv5@proton.me>
2025-12-01 20:16:55 +0900

    first commit XD

---
+++ 2025/Day1.py
@@ -0,0 +1,44 @@
+import os
+import csv
+
+file_path = "2025/day1.csv"
+# Import data
+def read_data(file_path):
+    with open(file_path, 'r') as f:
+        data = f.read().splitlines()
+    return data
+
+# Print an array row by row
+def print_array_rows(arr):
+    for row in arr:
+        print(row)
+
+data = read_data(file_path=file_path)
+
+# L68
+# L30
+# R48
+# L5
+# R60
+# L55
+# L1
+# L99
+# R14
+# L82
+
+current_lock = 50   # Starting dial
+password = 0        # Pass
+for command in data:    
+    print(command)
+    sequence = command.strip("LR")      # Sequence = fucking number 19238129371283719283
+    if command.startswith('L'):         # Command = Left or right +-------==-=-=+-
+        current_lock = current_lock - int(sequence)
+        current_lock = current_lock % -100
+    elif command.startswith('R'):
+        current_lock = current_lock + int(sequence)
+        current_lock = current_lock % 100
+    print(current_lock)
+    if current_lock == 0:
+        password+=1
+print(password)
+
